ac56983c50ca0ed0ee3e7839b0da149ea68a3d37
Tomofumi Takata <tomofumi.takata@gmail.com>
2016-03-25 14:55:20 +0900

    Sogo3 commit

---
+++ Sogo3.py
@@ -0,0 +1,28 @@
+# coding: UTF-8
+
+import urllib2
+
+
+print "データをダウンロード中です..."
+wagahai = urllib2.urlopen("http://www.aozora.gr.jp/cards/000148/files/789_14547.html")
+
+wagahai2 = wagahai.read()
+wagahai3 = unicode(wagahai2, "shift-jis", errors = "ignore")
+
+
+
+def wordcount(data, w):
+	n = data.count(w)
+	print "{0}は{1}回でてきました。\n".format(w,n)
+
+while True:
+	print "「吾輩は猫である」の中で探したい言葉を入力してください。"
+	print "検索を中止するときは「おわり」と入力してください。"
+
+	word = raw_input()
+	if word == "おわり":
+		print "ありがとうございました。"
+		quit()
+	
+	else:
+		wordcount(data=wagahai3, w=word)
---
+++ test2.py
@@ -0,0 +1,4 @@
+# coding: UTF-8
+
+import urllib2
+data = urllib2.urlopen("http://www.python.org")
